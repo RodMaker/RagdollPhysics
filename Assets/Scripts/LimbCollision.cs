c6f55323717110692997d79c3e0612b9b23b710d
RodMaker <83619630+RodMaker@users.noreply.github.com>
2023-01-14 15:49:55 +0000

    14012023

---
+++ Assets/Scripts/LimbCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimbCollision : MonoBehaviour
+{
+    public PlayerController playerController;
+
+    void Start()
+    {
+        playerController = GameObject.FindObjectOfType<PlayerController>().GetComponent<PlayerController>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        playerController.isGrounded = true;
+    }
+}
